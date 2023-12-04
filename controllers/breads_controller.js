307f44bbaa121837cde01221f8a4683a39a950b3
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2023-12-04 00:38:57 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,14 +2,38 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// index
+
+// INDEX
 breads.get('/', (req, res) => {
-  res.render('index',
+  res.render('Index',
     {
       breads: Bread
     }
   )
-    // res.send(Bread)
+// res.send(Bread)
+})
+
+
+// show
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]){
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+//create  
+breads.post('/', (req, res) => {
+  if (req.body.hasGlutten === 'on') {
+    req.body.hasGlutten === 'true'
+  } else {
+    req.body.hasGlutten = 'false'
+  }
+  Bread.push(req.body)
+  res.send(Bread)
 })
 
 module.exports = breads
--- server.js
+++ server.js
@@ -16,11 +16,21 @@ app.get('/', (req, res) => {
   app.set('views', __dirname + '/views')
   app.set('view engine', 'jsx')
   app.engine('jsx', require('express-react-views').createEngine())
+  app.use(express.static('public'))
   
+ 
+
+
+
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
-  
+
+    // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 //listen
 app.listen(PORT, () => {
     console.log('listening on port, PORT');
